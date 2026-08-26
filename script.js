6be14a1d2837d1ad1070818a304af7fd29f1be8b
sendyprasdio <sendyprasdio63@gmail.com>
2026-08-26 15:53:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -66,3 +66,18 @@ window.addEventListener("scroll", () => {
     });
 
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const menuToggle = document.getElementById("menuToggle");
+    const navbar = document.querySelector(".navbar");
+
+    menuToggle.addEventListener("click", function () {
+
+        navbar.classList.toggle("active");
+
+    });
+
+});
+
